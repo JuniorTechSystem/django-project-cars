2b2fbdc5968feeba5acbb17288823782e7ac75ec
Junior Bortolanza <gbortolanzajr@gmail.com>
2024-08-04 12:44:23 -0300

    Creates url cars/ and view cars_view

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def cars_view(request):
+    html = '''
+            <html>
+                <head>
+                    <title>My Cars</title>
+                </head>
+                <body>
+                    <h1>Cars of Junior</>
+                    <h3> The best cars!</h3> 
+                </body>
+            </html>
+    '''
+    return HttpResponse(html) 
