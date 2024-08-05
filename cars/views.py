2cdc675b32ffce2e868ed22795b1420e0341f2ec
Junior Bortolanza <gbortolanzajr@gmail.com>
2024-08-04 21:05:56 -0300

    Created templates folder and file cars.html

--- cars/views.py
+++ cars/views.py
@@ -1,15 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def cars_view(request):
-    html = '''
-            <html>
-                <head>
-                    <title>My Cars</title>
-                </head>
-                <body>
-                    <h1>Cars of Junior</>
-                    <h3> The best cars!</h3> 
-                </body>
-            </html>
-    '''
-    return HttpResponse(html) 
+    return render(request, 'cars.html') 
